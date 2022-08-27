652167e625bf0e86dc13b6d23c22d0ff1b4628b9
William Owen <william.owen.bsc@gmail.com>
2022-08-26 17:04:59 -0700

    other changes

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function PageNotFound() {
+    return (
+        <PageNotFoundStyled>
+            PageNotFound
+        </PageNotFoundStyled>
+    );
+}
+
+const PageNotFoundStyled = styled.div``;
---
+++ src/pages/story/Story.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function Story() {
+    // alert("hi");
+    return <StoryStyled>Story</StoryStyled>;
+}
+
+const StoryStyled = styled.div``;
