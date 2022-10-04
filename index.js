c9724a0d31a7667e7254d04bb3e5a1981d7e3a1c
William Owen <william.owen.bsc@gmail.com>
2022-10-04 09:35:26 -0700

    added a catchall

--- index.js
+++ index.js
@@ -5,11 +5,19 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(
-  express.static(
-    path.join(__dirname, "dist")
-  )
+    express.static(path.join(__dirname, "dist"))
 );
+app.get("/*", function (req, res) {
+    res.sendFile(
+        path.join(__dirname, "dist/index.html"),
+        function (err) {
+            if (err) {
+                res.status(500).send(err);
+            }
+        }
+    );
+});
 
 app.listen(PORT, () => {
-  console.log("listening on ", PORT);
+    console.log("listening on ", PORT);
 });
